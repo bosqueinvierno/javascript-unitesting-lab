d857da7766e83538549150622f1d37ccb1f48171
Leonardo Micheloni <leonardo@kinetica-solutions.com>
2014-10-20 21:08:01 -0300

    probando hook

--- ejemplos/ejemplo12.bat
+++ ejemplos/ejemplo12.bat
@@ -1,16 +1,22 @@
 rem ejecutar test
-echo ejecutando tests
-phantomjs ejemplo12.js ejemplo12.html
+echo %1
+
+echo %2
+echo %3
+echo %4
+pause
+REM echo ejecutando tests
+phantomjs ejemplos/ejemplo12.js ejemplos/ejemplo12.html
 
 
-IF NOT ERRORLEVEL 1 GOTO DONE
-  ECHO  An error occurred with exit code 1 or higher.
-  PAUSE
-  EXIT
-:DONE
+REM IF NOT ERRORLEVEL 1 GOTO DONE
+  REM ECHO  An error occurred with exit code 1 or higher.
+  REM PAUSE
+  REM EXIT
+REM :DONE
 
-rem minificar
-rem copiar en carpeta de salida
-echo minificando...
-mkdir build
-uglifyjs ejemplo12.js -o build\package.js
+minificar
+copiar en carpeta de salida
+REM echo minificando...
+REM mkdir build
+REM uglifyjs ejemplo12.js -o build\package.js
